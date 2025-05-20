49f1a40a27a4ad76cddae4da395daa7b6454df65
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-05-20 12:58:14 -0300

    Update JogoDaAdivinhação.py

--- JogoDaAdivinhação.py
+++ JogoDaAdivinhação.py
@@ -1,4 +1,5 @@
 from random import randint
+
 computador = randint(0,10)
 print('Sou seu computador... Acabei de pensar em um número entre 0 e 10.')
 print('Será que você consegue adivinhar qual foi? ')
