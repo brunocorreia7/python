767b81516adb4a620a2560b49037c1c3f2765612
Bruno Correia <165749396+brunocorreia7@users.noreply.github.com>
2025-12-14 12:57:22 -0300

    Add financing duration input and calculate payment

--- Programa_financiar_casa.py
+++ Programa_financiar_casa.py
@@ -1,7 +1,9 @@
 casa = float(input('valor da casa: R$ '))
 
 salário = float(input('sálario do comprador: R$ '))
+
 anos = int(input('quantos anos de financiamento? '))
+
 prestação = casa / (anos * 12)
 mínimo = salário * 30 / 100
 print(f'para pagar uma casa de R${casa:.2f}em {anos}anos')
